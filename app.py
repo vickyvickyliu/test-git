a7147fbc8574d992c222fc4d1ef8f1845accfa0f
vicky vicky <09170101@gm.scu.edu.tw>
2021-06-14 21:08:56 +0800

    ok

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from message import *
 from new import *
 from Function import *
 from appnew import *
-from member import *
+#from member import *
 #======這裡是呼叫的檔案內容=====
 
 #======python的函數庫==========
