d168e0f0c142c2c88827588c64d523b03f91044f
vicky vicky <09170101@gm.scu.edu.tw>
2021-06-14 10:45:00 +0800

    ok

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ from linebot.models import *
 from message import *
 from new import *
 from Function import *
+from appnew import *
 #======這裡是呼叫的檔案內容=====
 
 #======python的函數庫==========
@@ -27,7 +28,7 @@ static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
 line_bot_api = LineBotApi('rJEleCsjWB6K1JbL7GEeuA97VtYBUjaKeav4nH+33ajx5rXuwFxQdjJ0PCqALVQPt3Mk6bZx6giFImad7eMFHEzJYnvB4ELf31UTr2yZ3HpZsHPz9+ppr7M5YtihtVGIFh0WWoSC4lyS0iDDcr3BiAdB04t89/1O/w1cDnyilFU=')
 # Channel Secret
 handler = WebhookHandler('e5656d0269c4c60d4d149288d67e3083')
-
+mydict={}
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -52,9 +53,9 @@ def handle_message(event):
         uu=event.source.user_id
         line_bot_api.push_message('Udf7af3efedecd6323e16491c202af7ac',TextSendMessage(text=uu+'發了:'+msg))
         try:
-            line_bot_api.multicast(['Udf7af3efedecd6323e16491c202af7ac', 'Ude8b39f4c814be323dba05addd90a40a'], TextSendMessage(text=uu+"發了:"+msg))
+            #line_bot_api.multicast(['Udf7af3efedecd6323e16491c202af7ac', 'Ude8b39f4c814be323dba05addd90a40a'], TextSendMessage(text=uu+"發了:"+msg))
         #uu=event.source.user_id
-        #line_bot_api.multicast(['Uea89b2572fdf1e317967abcc6adf9acc', 'U0b7c9d483a2832b52d89e7d6f8820284'], TextSendMessage(text=uu+"發了:"+msg))
+        #line_bot_api.multicast(['Udf7af3efedecd6323e16491c202af7ac', 'U0b7c9d483a2832b52d89e7d6f8820284','','','',''], TextSendMessage(text=uu+"發了:"+msg))
         except:
             message = TextSendMessage(text="錯誤")
             line_bot_api.reply_message(event.reply_token, message)  
@@ -62,26 +63,26 @@ def handle_message(event):
         message = TextSendMessage(text="錯誤")
         line_bot_api.reply_message(event.reply_token, message)
         #line_bot_api.push_message(uu,TextSendMessage(text=uu+msg))
-    if '最新合作廠商' in msg:
-        message = imagemap_message()
+    if '會員' in msg:
+        message = membersystem()
         line_bot_api.reply_message(event.reply_token, message)
-    elif '最新活動訊息' in msg:
+    elif '揪車情況/取消揪車' in msg:
         message = buttons_message()
         line_bot_api.reply_message(event.reply_token, message)
-    elif '註冊會員' in msg:
+    elif '即時資訊' in msg:
         message = Confirm_Template()
         line_bot_api.reply_message(event.reply_token, message)
-    elif '旋轉木馬' in msg:
+    elif '我要揪車' in msg:
         message = Carousel_Template()
         line_bot_api.reply_message(event.reply_token, message)
-    elif '圖片畫廊' in msg:
+    elif '尋找共乘' in msg:
         message = test()
         line_bot_api.reply_message(event.reply_token, message)
-    elif '功能列表' in msg:
+    elif '使用說明/QA/其他' in msg:
         message = function_list()
         line_bot_api.reply_message(event.reply_token, message)
     else:
-        message = TextSendMessage(text="成功")
+        message = TextSendMessage(text="請點選圖文表單上的功能，進入服務喔！")
         line_bot_api.reply_message(event.reply_token, message)
 
 import os
---
+++ appnew.py
@@ -0,0 +1,185 @@
+from linebot import (LineBotApi, WebhookHandler)
+from linebot.exceptions import (InvalidSignatureError)
+from linebot.models import *
+
+def membersystem():
+    message = TemplateSendMessage(
+        alt_text='會員按鈕',
+        template=CarouselTemplate(
+            columns=[
+                CarouselColumn(
+                    thumbnail_image_url='https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Number_1_in_green_rounded_square.svg/200px-Number_1_in_green_rounded_square.svg.png',
+                    title='會員功能',
+                    text='請選擇服務',
+                    actions=[
+                        PostbackTemplateAction(
+                            label='註冊',
+                            data='註冊',
+                            text='12345'
+                        ),
+                        PostbackTemplateAction(
+                            label='登入',
+                            data='登入',
+                            text='登入'
+                        ),
+                        PostbackTemplateAction(
+                            label=' ',
+                            data=' ',
+                            text=' '
+                        )
+                    ]
+                ),
+                CarouselColumn(
+                    thumbnail_image_url='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuo7n2_HNSFuT3T7Z9PUZmn1SDM6G6-iXfRC3FxdGTj7X1Wr0RzA',
+                    title='會員功能',
+                    text='請選擇服務',
+                    actions=[
+                        PostbackTemplateAction(
+                            label='更改密碼/忘記密碼',
+                            data='更改密碼/忘記密碼',
+                            text='更改密碼/忘記密碼'
+                        ),
+                        PostbackTemplateAction(
+                            label='查看歷史紀錄',
+                            data='查看歷史紀錄',
+                            text='查看歷史紀錄'
+                        ),
+                        PostbackTemplateAction(
+                            label='查看信用值',
+                            data='查看信用值',
+                            text='查看信用值'
+                        )
+                    ]
+                )
+            ]
+        )
+    )
+    return message
+
+#TemplateSendMessage - ImageCarouselTemplate(圖片旋轉木馬)
+
+def cancelcheck():
+    message = TemplateSendMessage(
+        alt_text='是否確定取消共乘？',
+        template=ConfirmTemplate(
+            text="是否確定取消共乘？",
+            actions=[
+                PostbackTemplateAction(
+                    label="確定取消",
+                    text="確定取消",
+                    data="確定取消"
+                ),
+                PostbackTemplateAction(
+                    label="放棄取消",
+                    text="放棄取消",
+                    data="放棄取消"
+                )
+            ]
+        )
+    )
+    return message
+
+def chooseleadtype():
+    message = TemplateSendMessage(
+        alt_text='請選擇發車類別',
+        template=ConfirmTemplate(
+            text="請選擇發車類別",
+            actions=[
+                PostbackTemplateAction(
+                    label="1.確定要發車(已訂車者)",
+                    text="sdjiofvnfdjksfjfl",
+                    data="1"
+                ),
+                PostbackTemplateAction(
+                    label="2.滿員後叫車(等人數湊齊才會訂車)",
+                    text="2",
+                    data="2"
+                )
+            ]
+        )
+    )
+    return message
+
+def chooselocation():
+    message = TemplateSendMessage(
+        alt_text='請選擇目弟弟',
+        template=ButtonsTemplate(
+            text="請選擇目的地",
+            actions=[
+                PostbackTemplateAction(
+                    label="1.捷運士林站",
+                    text="目的地:捷運士林站",
+                    data="1"
+                ),
+                PostbackTemplateAction(
+                    label="2.捷運劍南路站",
+                    text="目的地:捷運劍南路站",
+                    data="2"
+                ),
+                PostbackTemplateAction(
+                    label="3.東吳大學外雙溪校區",
+                    text="目的地:東吳大學外雙溪校區",
+                    data="3"
+                ),
+                PostbackTemplateAction(
+                    label="4.東吳大學城中校區",
+                    text="目的地:東吳大學城中校區",
+                    data="4"
+                )
+            ]
+        )
+    )
+    return message
+
+def checkifontime():
+    message = TemplateSendMessage(
+        alt_text='請確認是否上車(一分鐘內未按將視為跑單)',
+        template=ButtonsTemplate(
+            text="請確認是否上車\n(一分鐘內未按將視為跑單)",
+            actions=[
+                PostbackTemplateAction(
+                    label="我已上車",
+                    text="我已上車",
+                    data="我已上車"
+                ),
+                PostbackTemplateAction(
+                    label=" ",
+                    text=" ",
+                    data=" "
+                )
+            ]
+        )
+    )
+    return message 
+  
+def checknumber():
+    message = TemplateSendMessage(
+        alt_text='您要加入的共乘是編號xx，請確認',
+        template=ConfirmTemplate(
+            text="您要加入的共乘是編號xx，請確認",
+            actions=[
+                PostbackTemplateAction(
+                    label="是",
+                    text="是",
+                    data="是"
+                ),
+                PostbackTemplateAction(
+                    label="否",
+                    text="否",
+                    data="否"
+                )
+            ]
+        )
+    )
+    return message
+
+'''    dict{
+        "lineid":{
+            "我要揪車":{
+                "name": "劉語萱","學號":"09170101","揪車種類":"人滿再成團"
+            }
+        }
+    }
+def we(uu):
+    if dict[][register][]userid
+    a=dict[uu][register](name)'''
